344ad40856149cb6fdbe19164d83fc3b623222ed
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-23 15:46:46 +0500

    verify user

--- backend/src/utils/verifyUser.js
+++ backend/src/utils/verifyUser.js
@@ -10,12 +10,23 @@ cont decode=await jwt.verify(token,process.env.JWT_SECRET)
 //     const options =
 // }
 
-// const verifyUser = async (res, req) => {
-//     console.log("reqest", req.cookies);
-//     try { 
-
-//     } catch {
-        
-//      }
-// }
-// export default verifyUser
+const verifyUser = async (res, req) => {
+    console.log("reqest", req.cookies);
+    const token = req.cookies.jwtToken;
+    try {
+        const decode = await jwt.verify(token, process.env.JWT_SECRET)
+        if (decode) {
+            next()
+        }
+        else {
+            res.json({
+                data: "error"
+            })
+        }
+    } catch (error) {
+        res.json({
+            error: error.message
+        })
+    }
+}
+export default verifyUser
